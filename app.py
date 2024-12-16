d9ae585466bd25da0f8d04c6fe24d69ff101b378
erikerikerikerikerikerikerik <erikbruil@gmail.com>
2024-12-16 14:59:41 +0100

    lint

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+local-video app
+**Designed and coded by erikerikerikerikerikerikerik - Erik Bruil**
+"""
+
 import os
 import ffmpeg
 from flask import Flask, render_template, send_from_directory
@@ -5,7 +10,7 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__, static_folder='templates')
 
 PROJECT_ROOT = os.path.abspath('.')
-EXCLUDED_DIRS = ['templates', 'thumbnails']  
+EXCLUDED_DIRS = ['templates', 'thumbnails']
 THUMBNAILS_DIR = os.path.join(PROJECT_ROOT, 'thumbnails')
 SUPPORTED_EXTENSIONS = ('.mp4', '.avi', '.mov', '.mkv')
 
@@ -13,6 +18,7 @@ if not os.path.exists(THUMBNAILS_DIR):
     os.makedirs(THUMBNAILS_DIR)
 
 def generate_video_thumbnail(video_path):
+    """generate video thumbnail"""
     video_filename = os.path.basename(video_path)
     thumbnail_filename = os.path.splitext(video_filename)[0] + '.jpg'
     thumbnail_path = os.path.join(THUMBNAILS_DIR, thumbnail_filename)
@@ -21,19 +27,23 @@ def generate_video_thumbnail(video_path):
         return thumbnail_filename
 
     try:
-        ffmpeg.input(video_path, ss=1).output(thumbnail_path, vframes=1, vf="scale='if(gte(iw,ih),300,-1)':'if(gte(iw,ih),-1,300)'").run()
-    except Exception as e:
-        print(f"Error generating thumbnail for {video_filename}: {e}")
+        ffmpeg.input(video_path, ss=1)\
+            .output(thumbnail_path, vframes=1,\
+                    vf="scale='if(gte(iw,ih),300,-1)':'if(gte(iw,ih),-1,300)'")\
+            .run()
+    except ValueError:
+        print(f"Error generating thumbnail for {video_filename}")
         return None
-    
+
     return thumbnail_filename
 
 def find_videos(root_dir, excluded_dirs):
+    """find videos"""
     video_files = []
-    for root, dirs, files in os.walk(root_dir):
+    for root, _, files in os.walk(root_dir):
         if any(excluded_dir in root for excluded_dir in excluded_dirs):
             continue
-        
+
         for file in files:
             if file.lower().endswith(SUPPORTED_EXTENSIONS):
                 relative_path = os.path.relpath(os.path.join(root, file), root_dir)
@@ -42,6 +52,7 @@ def find_videos(root_dir, excluded_dirs):
 
 @app.route('/')
 def index():
+    """index /"""
     videos = find_videos(PROJECT_ROOT, EXCLUDED_DIRS)
     video_thumbnails = {}
 
@@ -53,14 +64,17 @@ def index():
 
 @app.route('/play/<path:filename>')
 def play_video(filename):
+    """play video"""
     return render_template('video_player.html', video_path=filename)
 
 @app.route('/video/<path:filename>')
 def serve_video(filename):
+    """serve video"""
     return send_from_directory(PROJECT_ROOT, filename)
 
 @app.route('/thumbnail/<path:filename>')
 def serve_thumbnail(filename):
+    """serve thumbnail"""
     return send_from_directory(THUMBNAILS_DIR, filename)
 
 if __name__ == '__main__':
