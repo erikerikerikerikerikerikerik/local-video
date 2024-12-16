fd1b280fdb84d7a8084fd604257629d523915844
erikerikerikerikerikerikerik <erikbruil@gmail.com>
2024-12-16 06:29:10 +0100

    adjusted dir path

--- app.py
+++ app.py
@@ -2,30 +2,35 @@ from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
-VIDEO_DIR = 'static/videos'
+
+PARENT_DIR = os.path.abspath(os.path.dirname(__file__))
+EXCLUDED_DIR = 'local-video'
 SUPPORTED_EXTENSIONS = ('.mp4', '.avi', '.mov', '.mkv')
 
-def find_videos(root_dir):
+def find_videos(parent_dir, excluded_dir):
     video_files = []
-    for root, _, files in os.walk(root_dir):
+    for root, dirs, files in os.walk(parent_dir):
+        if EXCLUDED_DIR in root:
+            continue
+        
         for file in files:
             if file.lower().endswith(SUPPORTED_EXTENSIONS):
-                relative_path = os.path.relpath(os.path.join(root, file), root_dir)
+                relative_path = os.path.relpath(os.path.join(root, file), parent_dir)
                 video_files.append(relative_path)
     return video_files
 
 @app.route('/')
 def index():
-    videos = find_videos(VIDEO_DIR)
+    videos = find_videos(PARENT_DIR, EXCLUDED_DIR)
     return render_template('index.html', videos=videos)
 
 @app.route('/play/<path:filename>')
 def play_video(filename):
     return render_template('video_player.html', video_path=filename)
 
-@app.route('/static/videos/<path:filename>')
+@app.route('/video/<path:filename>')
 def serve_video(filename):
-    return send_from_directory(VIDEO_DIR, filename)
+    return send_from_directory(PARENT_DIR, filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
